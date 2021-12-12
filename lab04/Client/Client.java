1679b92c64b2cfe863d457c361e27869110f04f6
Ale142 <ale.bacci142@gmail.com>
2021-12-12 17:56:17 +0100

    partial creation of file; handled error from Conversion

--- lab04/Client/Client.java
+++ lab04/Client/Client.java
@@ -2,6 +2,8 @@ package Client;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -14,6 +16,11 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
 
 public class Client {
     Client(String server, int port, String originalType, String targetType, String imagePath)
@@ -47,18 +54,45 @@ public class Client {
         System.out.println("File sent");
         fileIn.close();
 
-        // Read length of converted file
-        byte[] byteLengthConvertedFile = new byte[4];
-        in.readNBytes(byteLengthConvertedFile, 0, 4);
-        int lengthConvertedFile = ByteBuffer.wrap(byteLengthConvertedFile).getInt();
-        System.out.println("Length of converted File: " + lengthConvertedFile);
+        // Read status code
+        byte[] statusCodeBytes = new byte[4];
+        in.readNBytes(statusCodeBytes, 0, 4);
+        int statusCode = ByteBuffer.wrap(statusCodeBytes).getInt();
+
+        // Read length of response (it could be length of the file or of the error)
+        byte[] byteLengthResponse = new byte[4];
+        in.readNBytes(byteLengthResponse, 0, 4);
+        int lengthResponse = ByteBuffer.wrap(byteLengthResponse).getInt();
+        System.out.println("Length of response: " + lengthResponse);
+
+        // Check status code
+        if (statusCode != 0) {
+            byte[] error = new byte[lengthResponse];
+            in.readNBytes(error, 0, lengthResponse);
+            String errore = new String(error);
+            socket.close();
+            throw new Error(errore);
+        }
+
+        // Creation of converted file
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte[] fileConverted = new byte[1024];
         int count = 0;
-        while ((count += in.read(fileConverted)) > 0 && count < lengthConvertedFile) {
+        while ((count += in.read(fileConverted, 0, 1024)) > 0 && count < lengthResponse) {
             // TODO: create converted image;
             // HAndle errors
+            baos.write(fileConverted);
         }
+        System.out.println("Count:" + count);
         System.out.println("File received");
+
+        byte[] data = baos.toByteArray();
+        ByteArrayInputStream bis = new ByteArrayInputStream(data);
+        BufferedImage imageToCreate = ImageIO.read(bis);
+
+        ImageIO.write(imageToCreate, targetType, new File(imagePath.split(Pattern.quote("."))[0] + "." + targetType));
+
+        System.out.println("File created");
 
         out.close();
         in.close();
--- lab04/Server/ConverterService.java
+++ lab04/Server/ConverterService.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,7 +17,6 @@ import java.util.logging.Logger;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
-import javax.swing.text.StyledEditorKit;
 
 public class ConverterService implements Runnable {
     Socket socket;
@@ -68,38 +68,60 @@ public class ConverterService implements Runnable {
         byte length[] = new byte[4];
         in.readNBytes(length, 0, 4);
         int fileLength = ByteBuffer.wrap(length).getInt();
-        System.out.println("File's length:" + fileLength);
 
         // Read file bytes
         byte[] buffer = new byte[1024];
         int count = 0;
-        while ((count += in.read(buffer)) > 0 && count < fileLength) {
+        while ((count += in.read(buffer)) < fileLength) {
             baos.write(buffer);
         }
 
-        System.out.println("File received");
-        ByteArrayOutputStream baosImageToSend = new ByteArrayOutputStream();
-        byte[] bytes = baos.toByteArray();
-        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-        BufferedImage imageReceived = ImageIO.read(bais);
-        ImageIO.write(imageReceived, tType, baosImageToSend);
+        System.out.println("Count:" + count);
+        logger.log(Level.INFO, "File received with length:" + fileLength);
 
-        BufferedInputStream bisImageToSend = new BufferedInputStream(
-                new ByteArrayInputStream(baosImageToSend.toByteArray()));
-        int bufferSize = 1 * 1024; // 1KB
-        byte[] buffer2 = new byte[bufferSize];
+        // Conversion
         try {
+            ByteArrayOutputStream baosImageToSend = new ByteArrayOutputStream();
+            byte[] bytes = baos.toByteArray();
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            BufferedImage imageReceived = ImageIO.read(bais);
 
-            int fileConvertedLength = baosImageToSend.toByteArray().length;
-            System.out.println("File Converted Length: " + fileConvertedLength);
-            out.write(ByteBuffer.allocate(4).putInt(fileConvertedLength).array());
+            ImageIO.write(imageReceived, tType, new File("prova." + tType));
+            ImageIO.write(imageReceived, tType, baosImageToSend);
 
-            while ((bisImageToSend.read(buffer2, 0, bufferSize)) != -1) {
-                out.write(buffer2);
+            BufferedInputStream bisImageToSend = new BufferedInputStream(
+                    new ByteArrayInputStream(baosImageToSend.toByteArray()));
+            int bufferSize = 1 * 1024; // 1KB
+            byte[] buffer2 = new byte[bufferSize];
+
+            logger.log(Level.INFO, "File converted successfully");
+
+            // Send 0 if conversion goes well
+            out.write(ByteBuffer.allocate(4).putInt(0).array());
+
+            try {
+
+                // Send file Length
+                int fileConvertedLength = baosImageToSend.toByteArray().length;
+                logger.log(Level.INFO, "File converted length:" + fileConvertedLength);
+                out.write(ByteBuffer.allocate(4).putInt(fileConvertedLength).array());
+                count = 0;
+                while ((bisImageToSend.read(buffer2, 0, bufferSize)) != -1) {
+                    out.write(buffer2);
+                }
+
+                logger.log(Level.INFO, "File converted and sent to the client");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            System.out.println("File converted and sent to the client");
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            String errorMessage = e.getMessage();
+            out.write(ByteBuffer.allocate(4).putInt(1).array());
+            // Send length of message
+            int lengthErrorMessage = errorMessage.getBytes().length;
+            out.write(ByteBuffer.allocate(4).putInt(lengthErrorMessage).array());
+            out.write(errorMessage.getBytes());
+
         }
         // while ((c = in.read()) != -1) {
         // System.out.println("Received: " + (char) c);
